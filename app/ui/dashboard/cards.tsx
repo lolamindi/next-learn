b9070682145d4dffed954a4c2712e2bebb1bddc0
lolamindi <laura_mday@yahoo.co.uk>
2024-01-18 12:26:10 +0100

    follow documentation

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -12,6 +13,7 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+
 
 export default async function CardWrapper() {
   return (
