1cf0e610f2fc7c62ae9b9d6381a6bce3a400631d
lolamindi <laura_mday@yahoo.co.uk>
2024-01-19 10:58:23 +0100

    add cards and data

---
+++ app/lib/actions.ts
@@ -0,0 +1,1 @@
+export async function createInvoice(formData: FormData) {}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,3 @@
-import { unstable_noStore as noStore } from 'next/cache';
-
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -11,22 +9,22 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  noStore();
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -196,6 +194,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
